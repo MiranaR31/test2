776ef589775a346a01781c48f818b878a17c7967
MiranaR31 <miranaranerison@hotmail.fr>
2026-01-28 11:44:17 +0100

    Update explicits.js

--- explicits.js
+++ explicits.js
@@ -29,8 +29,9 @@ define(['questAPI'], function(Quest){
         autoSubmit:'false',
         numericValues:'true',
         help: '<%= pagesMeta.number < 3 %>',
-        helpText: 'Astuce : Vous pouvez double-cliquer sur votre réponse pour passer à la question suivante. Attention, vous ne pourrez pas revenir sur votre réponse précédente.'
-
+        helpText: 'Astuce : Vous pouvez double-cliquer sur votre réponse pour passer à la question suivante.',
+		helpText: 'Attention, vous ne pourrez pas revenir sur votre réponse précédente.'
+	});
     API.addQuestionsSet('basicSelect',{ //Sélection unique 
         inherit :'basicQ',
         type: 'selectOne'
